ad468cbbfd441852bb24c9d3cae4f0dc177384a3
MarcoRamcharan <miguelmarcoramcharan@gmail.com>
2022-12-06 15:25:36 +0200

    fklmfkf

---
+++ src/AboutPage.jsx
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+    return ( 
+        <div className="aboutPage">
+            <h1>i am the about page</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate 
+                exercitationem est
+                ipsum quas, ut quia temporibus doloribus repellat voluptatem ullam.</p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
